9c83a718dda0308d33cbb8b66bbb9c5836694c6b
HIROTAKA <mhlabo@gmail.com>
2021-07-27 14:10:10 +0000

    kadai-ruby-2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
-  if num % 3 == 0 #数値が3の倍数であれば、戻り値は”Fizz”
+  if num % 3 == 0 && num % 5 == 0 #数値が3の倍数であり5の倍数でもある場合は、戻り値は”FizzBuzz”
+    'FizzBuzz'
+  elsif num % 3 == 0 #数値が3の倍数であれば、戻り値は”Fizz”
     'Fizz'
   elsif num % 5 == 0 #数値が5の倍数であれば、戻り値は”Buzz”
     'Buzz'
-  elsif num % 3 == 0 && num % 5 == 0 #数値が3の倍数であり5の倍数でもある場合は、戻り値は”FizzBuzz”
-    'FizzBuzz'
   else #上記のどれも満たさない場合は、その数値自体を戻り値にする
     num
   end
