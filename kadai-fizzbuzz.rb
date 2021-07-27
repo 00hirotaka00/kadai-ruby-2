e74747fbe850d2bfd4c29ae716153af8390ad080
HIROTAKA <mhlabo@gmail.com>
2021-07-27 13:39:55 +0000

    kadai-ruby-2

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(num)
+  if num % 3 == 0 #数値が3の倍数であれば、戻り値は”Fizz”
+    'Fizz'
+  elsif num % 5 == 0 #数値が5の倍数であれば、戻り値は”Buzz”
+    'Buzz'
+  elsif num % 3 == 0 && num % 5 == 0 #数値が3の倍数であり5の倍数でもある場合は、戻り値は”FizzBuzz”
+    'FizzBuzz'
+  else #上記のどれも満たさない場合は、その数値自体を戻り値にする
+    num
+  end
+end
+
+num_max = 100
+
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end
